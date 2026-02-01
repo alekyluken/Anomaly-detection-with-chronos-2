4ffa24dee25814fdb2c794ad0398c38dcede5043
Nuzz23 <nunzio.licalzi9@gmail.com>
2026-02-01 23:48:20 +0100

    empty dir

--- Nunzio/customRunAD.py
+++ Nunzio/customRunAD.py
@@ -334,11 +334,10 @@ def main(configuration:dict, name:str)->None:
     os.makedirs(out_initial_path, exist_ok=True)
 
     # Parameters
-
     pipeline = get_pipeline(device='cuda')
     print(f"Using device: {next(pipeline.model.parameters()).device}")
     
-    save_path = f"results_{max(0, max(int(fname.split('_')[1].split('.')[0]) for fname in os.listdir(out_initial_path) if fname.startswith("results_") and fname.endswith(".json"))) + 1}.json"
+    save_path = f"results_{max([int(fname.split('_')[1].split('.')[0]) for fname in os.listdir(out_initial_path) if fname.startswith("results_") and fname.endswith(".json")] + [0]) + 1}.json"
 
     if os.path.exists(os.path.join(out_initial_path, save_path)):
         with open(os.path.join(out_initial_path, save_path), 'r', encoding='utf-8') as f:
